aaa84343c4779748d805990a0be28553809ea2c9
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-18 19:34:46 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_3.0.3/HomeTask_3.0.3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int usernum, count0=0, digit,ndigits=0, sum=0, mid, choice;
+    cout << "Введите число: \n";
+    cin >> usernum;
+    do
+    {
+        cout << "Выберите действие: \n"
+            << "1. Посчитать количество цифр в заданном числе\n"
+            << "2. Посчитать сумму цифр числа \n"
+            << "3. Определить количество нулей в заданном числе\n"
+            << "4. Выход из программы\n";
+        cin >> choice;
+        if (choice < 1 || choice>4) {
+            cout << "Нет такого варианта!\n";
+            continue;
+
+        }
+
+
+
+        while (usernum > 0) {
+            digit = usernum % 10;
+            sum += digit;
+            ndigits++;
+            if (digit == 0) {
+                count0++;
+            }
+            usernum = usernum / 10;
+        }
+        mid = sum / ndigits;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Количество цифр в заданном числе = " << ndigits<<endl << endl;
+            break;
+        case 2:
+            cout << "Сумма цифр в заданном числе = " << sum << endl << endl;
+            break;
+        case 3:
+            cout << "Количество нулей в заданном числе = " << count0 << endl<<endl;
+            break;
+        
+        }
+
+    } while (choice != 4);
+}
